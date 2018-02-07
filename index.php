8c4c126d097a55cc6992162cb452f27a611285c1
DanielSchneiders <daniel.schneiders@hotmail.com>
2018-02-07 21:59:18 +0100

    First front-end

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>drawert</title>
+        <link type="text/css" rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+    	<header>
+        	<h1>Draw a <span id="draw_subject_name">...</span></h1>
+            <h3><span id="draw_screen_number">1</span> / <span id="draw_screen_total">5</span></h3>
+        </header>
+        <div id="wrapper">
+        	<div id="toolbar">
+             ...
+            </div>
+            <canvas>
+            </canvas>
+        </div>
+        <footer>
+        	<button id="next_button" type="button">Next drawing!</button>
+        </footer>
+    </body>
+</html>
