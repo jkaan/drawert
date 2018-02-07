8f0b0945d10ef3d73cdd1bb37c91fbf94cbac2db
Joey Kaan <jkaan@users.noreply.github.com>
2018-02-07 21:57:40 +0100

    Add a WIP of uploading a drawn image

--- server/src/public/index.php
+++ server/src/public/index.php
@@ -8,5 +8,6 @@ require __DIR__ . '/../../vendor/autoload.php';
 $app = new \Slim\App();
 
 $app->get('/startQuiz', StartQuizController::class . ':startQuiz');
+$app->post('/uploadDrawnImage', StartQuizController::class . ':uploadDrawnImage');
 
 $app->run();
