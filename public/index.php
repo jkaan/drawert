08effd29387c06e8fb7fef0fc3ece30ac55719d7
Joey Kaan <jkaan@users.noreply.github.com>
2018-02-20 21:29:54 +0100

    Add middleware for access control
    Output filename of moved file

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 
 use Drawert\Controller\StartQuizController;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -9,5 +11,9 @@ $app = new \Slim\App();
 
 $app->get('/startQuiz', StartQuizController::class . ':startQuiz');
 $app->post('/uploadDrawnImage', StartQuizController::class . ':uploadDrawnImage');
+
+$app->add(function(RequestInterface $request, ResponseInterface $response, callable $next) {
+    return $response->withHeader('Access-Control-Allow-Origin', '*');
+});
 
 $app->run();
