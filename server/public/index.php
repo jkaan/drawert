a733c94e53c82030b8478a308bd479301b0608e8
Joey Kaan <jkaan@users.noreply.github.com>
2018-02-20 19:15:56 +0100

    Fix moving of folder

--- server/public/index.php
+++ server/public/index.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Drawert\Controller\StartQuizController;
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App();
 
